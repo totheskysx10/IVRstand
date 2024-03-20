56ee5dd1a36fbe8e909cf850139a6cc8debfc5de
Totheskysx10 <crazycat87654@gmail.com>
2024-03-21 01:59:00 +0500

    Исправление ошибки в CategoryRepository

--- src/main/java/com/good/ivrstand/app/CategoryRepository.java
+++ src/main/java/com/good/ivrstand/app/CategoryRepository.java
@@ -5,7 +5,9 @@ import com.good.ivrstand.domain.Item;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     Category findById(long id);
     void deleteById(long id);
